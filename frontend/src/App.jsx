f1ff752c384e0e1c6a80b74ff9a37b24272107bf
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-20 15:49:00 +0530

    rendering data from useState arr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [jokes, setJokes] = useState([]);
 
   return (
     <>
       <h1>Full stack Dev</h1>
+      <p>JOKES: {jokes.length}</p>
+
+      {jokes.map((joke) => {
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>;
+      })}
     </>
   );
 }
