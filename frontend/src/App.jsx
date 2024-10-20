f4f5d53c9b0a9c26f0abfb1a970d90e0d3b50cf9
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-20 15:59:45 +0530

    hit backend route without cors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
   const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/jokes")
+      .then((res) => setJokes(res.data))
+      .catch((err) => console.log(err));
+  });
 
   return (
     <>
