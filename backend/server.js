653313b23c3cbdd3ef46d214fb3e909e3cec2219
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-17 00:23:57 +0530

    repo name

--- backend/server.js
+++ backend/server.js
@@ -49,5 +49,5 @@ app.get("/api/jokes", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is listening on http://localhost:${port}`);
 });
